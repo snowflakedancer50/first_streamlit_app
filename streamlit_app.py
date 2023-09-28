da279a36b1fd7727402fb9e846de3f29a4f324ef
snowflakedancer50 <144190680+snowflakedancer50@users.noreply.github.com>
2023-09-28 09:02:04 -0500

    Messed up indentation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ streamlist.header("The fruit load list contains:")
 #Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("select * from fruit_load_list")
-  return my_cur.fetchall()  
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()  
 
 if streamlist.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
